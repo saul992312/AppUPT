8633310054f76beebd37e6098d371c32d5668d38
Brandon <acostacordovabrandon@gmail.com>
2026-02-11 22:07:17 -0600

    Se crean las primeras 2 interfaces del generador de reportes

---
+++ app/src/main/java/com/example/appupt/Segunda_Interfaz_de_reporte.java
@@ -0,0 +1,24 @@
+package com.example.appupt;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Segunda_Interfaz_de_reporte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_segunda_interfaz_de_reporte);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
